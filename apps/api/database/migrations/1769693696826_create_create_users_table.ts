dff62aa1a08c0190cbe8b840b5549c164ff28d10
Antony <antony.huart37@gmail.com>
2026-01-29 15:29:29 +0100

    feat: add user table

---
+++ apps/api/database/migrations/1769693696826_create_create_users_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.bigInteger('github_id').unsigned().notNullable().unique()
+      table.string('username').notNullable()
+      table.string('email').notNullable().unique()
+      table.string('avatar_url').nullable()
+      table.jsonb('preferences').nullable()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
