1bb2d1710312448e3fb12ba81345b183e62dfe24
Antony <antony.huart37@gmail.com>
2026-04-23 21:49:08 +0200

    refactor(db): consolidate project colums into base migration

--- apps/api/database/migrations/1769693696826_create_create_users_table.ts
+++ apps/api/database/migrations/1769693696826_create_create_users_table.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
       table.increments("id");
       table.string("name").nullable();
       table.string("email").notNullable().unique();
-      table.string("access_token").nullable();
+      table.string("github_access_token").nullable();
       table.boolean("is_verified").defaultTo(false);
       table.string("avatar_url").nullable();
       table.jsonb("preferences").nullable();
